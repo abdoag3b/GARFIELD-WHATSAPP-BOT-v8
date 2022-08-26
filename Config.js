bd43cebd88ad49ad1a5da41029e9c8e4279c4a1f
abdoag3b <112177838+abdoag3b@users.noreply.github.com>
2022-08-26 15:31:44 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -17,22 +17,22 @@ global.emoji01 = ['🐼'] // 👈 Enter your Emoji
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
 *ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
-*From Sri Lanka 🇱🇰*
+*I am 14 years old developer...*
+*From EGYPT 🇪🇬*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
 *Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
+global.owner =  ['+201208536894'] //👈  Enter Your number
+global.premium =  ['+201208536894'] //👈  Enter Your number
+global.ownernomer = '+201208536894' //👈  Enter Your number
+global.ownername = 'عبضو 🤡' //👈 Enter Your name
+global.botname = '‌‌‌‌‌‌‌🇿‌🇪‌🇽 ‌‌‌🇧‌🇴‌🇹' //👈 Enter Your Bot name
+عالمي . تذييل  =  '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒𝖺𝗋𝖿𝗂𝖾𝖡𝗈𝗍'  // 👈 أدخل اسمك
 global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
 global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
-global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
-global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
+عالمي . sc  =  'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
+عالمي . fbt  =  'Follw Us ℹ️'  // 👈 أدخل (اشترك / تابع / أعجبني الآن)
 global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
 global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
 global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
